2dc3082e317df1e5a9c73b2ec3fbaf6206ebc3dc
Carla Balbontín Tanhnuz <cbalbontin@GST-CBALBON-MAC.local>
2019-10-30 19:49:03 -0300

    Reading from the store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { StoreModule } from '@ngrx/store';
 import { tutorialReducer } from './reducers/tutorial.reducer';
 import { ReadComponent } from './read/read.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReadComponent
+    ReadComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
