f36a1e97d469a1027e720090a9fdf67b4326ed32
carlabalbontin <carlabalbontin@hotmail.com>
2019-10-30 14:47:46 -0300

    Changes in folders, creation of read component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { StoreModule } from '@ngrx/store';
+import { tutorialReducer } from './reducers/tutorial.reducer';
+import { ReadComponent } from './read/read.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    StoreModule.forRoot({
+      tutorial: tutorialReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
